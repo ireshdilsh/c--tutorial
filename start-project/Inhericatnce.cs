f7ff9f4fba66761f001211c7013f7db4a01639d1
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-16 11:07:17 +0530

    update inhericatnce code

--- start-project/Inhericatnce.cs
+++ start-project/Inhericatnce.cs
@@ -3,9 +3,19 @@ using System;
 namespace start_project
 {
 
-// Inhericatnce java use extends keyword.but C# use : 
+    // Inhericatnce java use extends keyword.but C# use : 
     class Student : Person
     {
-        
+
+    }
+
+    class Inhericatnce
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Student();
+            person.setAge(45);
+            person.PrintInfo();
+        }
     }
 }
