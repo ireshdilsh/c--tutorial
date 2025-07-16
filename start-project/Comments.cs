9acbbe7ffeed80488206a4daa6b53b33bcfdbaaf
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-16 10:16:50 +0530

    create variables with data type

--- start-project/Comments.cs
+++ start-project/Comments.cs
@@ -4,12 +4,12 @@ namespace start_project
 {
     class Comments
     {
-        static void Main(string[] args)
-        {
-            // This is a comment (need prints number 1 only)
-            Console.WriteLine("Number 1");
-            // Console.WriteLine("Number 2");
-            Console.ReadLine();
-        }        
+        // static void Main(string[] args)
+        // {
+        //     // This is a comment (need prints number 1 only)
+        //     Console.WriteLine("Number 1");
+        //     // Console.WriteLine("Number 2");
+        //     Console.ReadLine();
+        // }        
     }
 }
---
+++ start-project/Variables.cs
@@ -0,0 +1,19 @@
+string name;
+int age;
+double weight;
+char gender;
+bool isAlive;
+
+name = "John Doe";
+age = 30;
+weight = 75.5;
+gender = 'M';
+isAlive = true;
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
+Console.WriteLine("Weight: " + weight);
+Console.WriteLine("Gender: " + gender);
+Console.WriteLine("Is alive: " + isAlive);
+
+Console.ReadLine();
