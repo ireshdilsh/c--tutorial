ce6165c2ea4c01f52ef8346fa2db464edfa7b181
ireshdilsh <ireshdilshan46@gmail.com>
2025-07-16 10:04:29 +0530

    create class using simple knowlwdge

--- start-project/Program.cs
+++ start-project/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello World!");
+using System;
+
+namespace start_project{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
